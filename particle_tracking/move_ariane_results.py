473f17620bb6bcdf99e54d1deb4cb4b06412d3c6
Doug Latornell <djl@douglatornell.ca>
2017-03-04 16:10:21 -0500

    Add directory tree for Ariane results management exercise.

---
+++ particle_tracking/move_ariane_results.py
@@ -0,0 +1,18 @@
+"""Demo of argparse command-line help.
+"""
+import argparse
+
+
+def main():
+    args = cmdline_parser()
+
+
+def cmdline_parser():
+    parser = argparse.ArgumentParser(
+        description='Description of script for help display'
+    )
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    main()
